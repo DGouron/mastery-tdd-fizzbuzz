6797611251ae852f6efdd33f00d5ddcba474d1bf
dgouron <damien.gouron@gmail.com>
2024-12-02 08:38:06 +0100

    feat: add fizzbuzz

--- src/fizzbuzz.test.ts
+++ src/fizzbuzz.test.ts
@@ -1,1 +1,17 @@
-// Write your test here
+import { fizzbuzz } from "./fizzbuzz.js";
+
+test('If the number is divisible by 3, return "Fizz"', () => {
+	expect(fizzbuzz(6)).toBe("Fizz");
+});
+
+test('If the number is divisible by 5, return "Buzz"', () => {
+	expect(fizzbuzz(10)).toBe("Buzz");
+});
+
+test('If the number is divisible by 3 and 5, return "FizzBuzz"', () => {
+	expect(fizzbuzz(15)).toBe("FizzBuzz");
+});
+
+test("If the number is not divisible by 3 or 5, return the number", () => {
+	expect(fizzbuzz(2)).toBe("2");
+});
